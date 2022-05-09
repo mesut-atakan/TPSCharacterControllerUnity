e73d3fd8c54dfdf77e111f8a112dcd4014f29e88
mesut_atakan <mesut_atakan5@hotmail.com>
2022-05-09 16:11:57 +0300

    characterController

--- CharacterControllerBETA/Scripts/PlayerController2.cs
+++ CharacterControllerBETA/Scripts/PlayerController2.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(Rigidbody))] //geçerli objede rigidbody yok ise rigidbody eklenecek.
-//[RequireComponent (typeof(CharacterController))] //geçerli objede Character Controller componenti bulunmuyorsa. Bu component eklenecek.
+//[RequireComponent (typeof(Rigidbody))] //geçerli objede rigidbody yok ise rigidbody eklenecek.
+[RequireComponent (typeof(CharacterController))] //geçerli objede Character Controller componenti bulunmuyorsa. Bu component eklenecek.
 public class PlayerController2 : MonoBehaviour
 {
-    private Rigidbody _rb;
+    //private Rigidbody _rb;
     private CharacterController controller;
 
     //speeds:
@@ -15,10 +15,16 @@ public class PlayerController2 : MonoBehaviour
     [SerializeField] [Tooltip("Run Speed")] private float _runSpeed;
     [SerializeField] [Tooltip("Crounch Speed")] private float _crounchSpeed;
     [SerializeField] [Tooltip("Jump Speed")] private float _jumpSpeed;
+    private float turnSmoothVelocity;
+    
+    [Space]
+    [Header("Transform")]
+    
+    [SerializeField] [Tooltip("Player Rotation Speed")] [Range(0f,1f)] private float turnSmoothTime = 0.1f;
 
     private void Awake()
     {
-        _rb = this.gameObject.GetComponent<Rigidbody>();
+        //_rb = this.gameObject.GetComponent<Rigidbody>();
         controller = this.gameObject.GetComponent<CharacterController>();
     }
 
@@ -29,13 +35,23 @@ public class PlayerController2 : MonoBehaviour
     
     private void CharacterController()
     {
-        _playerSpeed = Input.GetKey(KeyCode.LeftShift) ? 10 : !Input.GetKey(KeyCode.LeftControl) ? 5 : 2 ;
+        _playerSpeed = Input.GetKey(KeyCode.LeftShift) ? 4f : !Input.GetKey(KeyCode.LeftControl) ? 2.5f : 1f ;
 
         //character Movement
         float speed_x = _playerSpeed * Input.GetAxisRaw("Horizontal");
         float speed_z = _playerSpeed * Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(speed_x, 0f, speed_z);
 
-        _rb.velocity = new Vector3(speed_x, 0, speed_z);
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * _playerSpeed * Time.deltaTime);
+        }
+        
+        //_rb.velocity = new Vector3(speed_x, 0, speed_z);
     }
 }
 
